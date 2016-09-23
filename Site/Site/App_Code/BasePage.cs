ae71e573e7591e31677b81b09169f37f4e4f3634
Michael E Miles <michael@miles-me.net>
2016-09-23 12:53:53 -0500

    Started BaseBage Class

--- Site/Site/App_Code/BasePage.cs
+++ Site/Site/App_Code/BasePage.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
 
-namespace Site.App_Code
+////May have to get rid of this namespace. I'll comment out first
+//namespace Site.App_Code
+//{
+//    public class BasePage
+//    {
+//    }
+//}
+
+public class BasePage : System.Web.UI.Page
 {
-    public class BasePage
+    private void Page_PreRender(object sender, EventArgs e)
     {
+        
     }
 }
