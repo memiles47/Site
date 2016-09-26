29437d1eeb6240091967c107884f8c65cb8e5f40
Michael E Miles <michael@miles-me.net>
2016-09-26 07:54:23 -0500

    Complete BasePage Code

--- Site/Site/App_Code/BasePage.cs
+++ Site/Site/App_Code/BasePage.cs
@@ -15,6 +15,14 @@ public class BasePage : System.Web.UI.Page
 {
     private void Page_PreRender(object sender, EventArgs e)
     {
-        
+        if (string.IsNullOrEmpty(this.Title) || this.Title.Equals("Untitled Page", StringComparison.CurrentCultureIgnoreCase))
+        {
+            throw new Exception("Page Title cannot be \"Untitled Page\" or an empty string.");
+        }
+    }
+
+    public BasePage()
+    {
+        this.PreRender += Page_PreRender;
     }
 }
