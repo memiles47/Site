bbed5911eec49748f664097671e0b261de6e50d7
Michael E Miles <michael@miles-me.net>
2016-10-07 11:30:14 -0500

    Adding theme selection to the master page

--- Site/Site/MasterPages/Frontend.Master.designer.cs
+++ Site/Site/MasterPages/Frontend.Master.designer.cs
@@ -38,5 +38,14 @@ namespace Site.MasterPages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder cpMainContent;
+        
+        /// <summary>
+        /// ThemeList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ThemeList;
     }
 }
