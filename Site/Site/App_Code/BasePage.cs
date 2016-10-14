8336b5f372315e82da48075635ecf5663c1751ad
Michael E Miles <michael@miles-me.net>
2016-10-14 12:25:41 -0500

    Add change theme code for user

--- Site/Site/App_Code/BasePage.cs
+++ Site/Site/App_Code/BasePage.cs
@@ -19,9 +19,24 @@ public class BasePage : System.Web.UI.Page
         }
     }
 
+    private void Page_PreInit(object sender, EventArgs e)
+    {
+        var preferredTheme = Request.Cookies.Get("PreferredTheme");
+        if (preferredTheme != null)
+        {
+            var folder = Server.MapPath("~/App_Themes/" + preferredTheme.Value);
+
+            if (System.IO.Directory.Exists(folder))
+            {
+                Page.Theme = preferredTheme.Value;
+            }
+        }
+    }
+
     public BasePage()
     {
         this.PreRender += Page_PreRender;
+        this.PreInit += Page_PreInit;
     }
 }
 
