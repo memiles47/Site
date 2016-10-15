ee90031f0145cb1ed7c05aa3f8823f82fc201882
Michael E Miles <michael@miles-me.net>
2016-10-15 09:49:31 -0500

    Make changes recommended by ReSharper

--- Site/Site/App_Code/BasePage.cs
+++ Site/Site/App_Code/BasePage.cs
@@ -8,22 +8,23 @@
 //    }
 //}
 
-public class BasePage : System.Web.UI.Page
+namespace Site
 {
-    private void Page_PreRender(object sender, EventArgs e)
+    public class BasePage : System.Web.UI.Page
     {
-        if (string.IsNullOrEmpty(this.Title) ||
-            this.Title.Equals("Untitled Page", StringComparison.CurrentCultureIgnoreCase))
+        private void Page_PreRender(object sender, EventArgs e)
         {
-            throw new Exception("Page title cannot be \"Untitled Page\" or an empty string.");
+            if (string.IsNullOrEmpty(Title) ||
+                Title.Equals("Untitled Page", StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new Exception("Page title cannot be \"Untitled Page\" or an empty string.");
+            }
         }
-    }
 
-    private void Page_PreInit(object sender, EventArgs e)
-    {
-        var preferredTheme = Request.Cookies.Get("PreferredTheme");
-        if (preferredTheme != null)
+        private void Page_PreInit(object sender, EventArgs e)
         {
+            var preferredTheme = Request.Cookies.Get("PreferredTheme");
+            if (preferredTheme == null) return;
             var folder = Server.MapPath("~/App_Themes/" + preferredTheme.Value);
 
             if (System.IO.Directory.Exists(folder))
@@ -31,12 +32,12 @@ public class BasePage : System.Web.UI.Page
                 Page.Theme = preferredTheme.Value;
             }
         }
-    }
 
-    public BasePage()
-    {
-        this.PreRender += Page_PreRender;
-        this.PreInit += Page_PreInit;
+        protected BasePage()
+        {
+            PreRender += Page_PreRender;
+            PreInit += Page_PreInit;
+        }
     }
 }
 
