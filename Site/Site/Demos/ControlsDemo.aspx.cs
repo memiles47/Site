d75c21f63b129d77bbc52a52a7b1c282f11f0448
Michael E Miles <michael@miles-me.net>
2016-10-25 08:13:15 -0500

    Denial of Service attack caused me to miss a push this week.

--- Site/Site/Demos/ControlsDemo.aspx.cs
+++ Site/Site/Demos/ControlsDemo.aspx.cs
@@ -8,7 +8,7 @@ namespace Site.Demos
         {
 
         }
-
+        //Denial of service attack caused me to miss a push to GIT
         protected void btn_Submit_Click(object sender, EventArgs e)
         {
             lbl_Result.Text = "Your name is: " + txt_MichaelMiles.Text;
