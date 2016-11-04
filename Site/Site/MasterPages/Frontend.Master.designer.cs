8c49421287f1873a48aeaa1090fdb6fc3551089e
Michael E Miles <michael@miles-me.net>
2016-11-04 05:27:10 -0500

    Creating Menu across the top.

--- Site/Site/MasterPages/Frontend.Master.designer.cs
+++ Site/Site/MasterPages/Frontend.Master.designer.cs
@@ -31,6 +31,24 @@ namespace Site.MasterPages {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+        
+        /// <summary>
+        /// SiteMapDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+        
+        /// <summary>
         /// cpMainContent control.
         /// </summary>
         /// <remarks>
